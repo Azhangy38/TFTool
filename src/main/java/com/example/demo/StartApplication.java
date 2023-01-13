ce8c7c0bf4b6ebcfac288100975a69561366254f
Azhangy38 <33067558+Azhangy38@users.noreply.github.com>
2023-01-13 18:43:14 -0500

    Removed unnecessary files and update some Class names

---
+++ src/main/java/com/example/demo/StartApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+
+@SpringBootApplication
+public class StartApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(StartApplication.class, args);
+		System.out.println("Server is running");
+
+	}
+
+
+
+}
+
